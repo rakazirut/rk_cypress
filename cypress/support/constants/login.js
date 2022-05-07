71f805be34f20695f51b974edadd2c304927e332
Rob Kazirut <robert.kazirut@gmail.com>
2022-05-06 20:42:58 -0400

    alphabetize const files

--- cypress/support/constants/login.js
+++ cypress/support/constants/login.js
@@ -1,24 +1,26 @@
 const login_verbiage = {
-    usernames: {
-        standard: 'standard_user',
-        locked: 'locked_out_user',
-        problem: 'problem_user',
-        glitch: 'performance_glitch_user'
+    error_msg: {
+        locked: 'Epic sadface: Sorry, this user has been locked out.',
+        password: 'Epic sadface: Password is required',
+        username: 'Epic sadface: Username is required',
+        wrongpass: 'Epic sadface: Username and password do not match any user in this service'
     },
     password: 'secret_sauce',
-    username_error_msg: 'Epic sadface: Username is required',
-    password_error_msg: 'Epic sadface: Password is required',
-    locked_error_msg: 'Epic sadface: Sorry, this user has been locked out.',
-    wrongpass_error_msg: 'Epic sadface: Username and password do not match any user in this service'
+    usernames: {
+        glitch: 'performance_glitch_user',
+        locked: 'locked_out_user',
+        problem: 'problem_user',
+        standard: 'standard_user'
+    }
 }
 
 const login_elements = {
-    user_field: '[data-test=username]',
-    pass_field: '[data-test=password]',
+    error_msg: '.error-message-container',
     login_button: '[data-test=login-button]',
-    mascot_img: '.bot_column',
     login_logo: '.login_logo',
-    error_msg: '.error-message-container'
+    mascot_img: '.bot_column',
+    pass_field: '[data-test=password]',
+    user_field: '[data-test=username]',
 }
 
 export {
--- cypress/support/constants/product.js
+++ cypress/support/constants/product.js
@@ -1,6 +1,9 @@
 const product_elements = {
     top_nav: {
+        app_logo: '.app_logo',
+        nav_mascot: '.peek',
         hamburger_menu: {
+            close_btn: '.bm-cross-button',
             main_menu_button: '.bm-burger-button',
             menu_sidebar: '.bm-menu',
             menu_option: '.menu-item',
@@ -9,19 +12,17 @@ const product_elements = {
                 about: 'about_sidebar_link',
                 logout: 'logout_sidebar_link',
                 reset: 'reset_sidebar_link'
-            },
-            close_btn: '.bm-cross-button'
+            }
         },
-        app_logo: '.app_logo',
         shopping_cart: {
-            icon: '.shopping_cart_link',
-            title: '.title',
-            continue_btn: '[data-test=continue-shopping]',
-            checkout_btn: '[data-test=checkout]',
             cart_item: '.cart_item',
             cart_quantity: '.cart_quantity',
             cart_item_label: '.cart_item_label',
-            cart_remove_btn: '.cart_button'
+            cart_remove_btn: '.cart_button',
+            checkout_btn: '[data-test=checkout]',
+            continue_btn: '[data-test=continue-shopping]',
+            icon: '.shopping_cart_link',
+            title: '.title'
         },
         sort_dropdown: {
             container: '.product_sort_container',
@@ -31,24 +32,23 @@ const product_elements = {
                 'lohi',
                 'hilo'
             ] 
+        }
+    },
+    footer: {
+        footer_mascot: '.footer_robot',
+        social: {
+            social_container: '.social',
+            social_fb: '.social_facebook',
+            social_linkd: '.social_linkedin',
+            social_twit: '.social_twitter',
         },
-        nav_mascot: '.peek'
     },
     inventory: {
         add_cart_btn: '.btn_inventory',
         inventory_container: '.inventory_container',
-        inventory_list: '.inventory_list',
         inventory_item: '.inventory_item',
         inventory_item_name: '.inventory_item_name',
-    },
-    footer: {
-        social: {
-            social_container: '.social',
-            social_twit: '.social_twitter',
-            social_fb: '.social_facebook',
-            social_linkd: '.social_linkedin'
-        },
-        footer_mascot: '.footer_robot'
+        inventory_list: '.inventory_list'
     }
 }
 
